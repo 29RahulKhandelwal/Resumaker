e2beb19bc059d0a5c5d5c10b9a03cddd2b6f2c9e
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-04-11 15:29:24 +0530

    issue resolved

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import Header from "./HeaderFooter/Header";
-import Home from "../components/HomePage/Home";
-import TemplatePage from "./TemplatePage/TemplatePage";
+import Home from "./HomeSection/Home";
+import TemplatePage from "./TemplateSection/TemplatePage";
 import Footer from "./HeaderFooter/Footer";
 
 export default function App() {
