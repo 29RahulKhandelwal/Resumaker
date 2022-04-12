972ccca8ece375d20dc8bd20954f93f2908968e2
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-04-12 11:49:47 +0530

    Responsive

--- src/components/CreateResume/CreateResume.jsx
+++ src/components/CreateResume/CreateResume.jsx
@@ -8,14 +8,14 @@ export default function CreateResume() {
     return (
         <Fragment>
             <div class="row">
-                <div class="col col-lg-1 col-border">
+                <div class="col col-lg-1 col-border col-display">
                     <SideLinks />
                     <Button type="button" class="btn btn-center btn-outline-primary btn-md" text="MAKE" />
                 </div>
-                <div class="col col-lg-5 col-border scroll-bar">
+                <div class="col col-lg-5 col-md-12 col-border scroll-bar">
                     <Template />
                 </div>
-                <div class="col col-lg-6 col-border">
+                <div class="col col-lg-6 col-border col-display">
                     Column
                 </div>
             </div>
